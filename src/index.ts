3b36f841bc0e7b6460fc92ca6d516983457c6087
yasen-angelov <jasen_angelov@abv.bg>
2025-08-12 16:57:07 +0300

    Simplify Workout DTO exports; bump shared-dtos version to 1.0.17

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 export * from './Users/user.dto';
-
 export * from './Auth/login-request.dto';
 export * from './Auth/login-response.dto';
-
 export * from './Utility/error-response.dto';
 export * from './Excersises/ExcersiseDto';
 export * from './Excersises/create.exersise.request';
 export * from './Utility/enlistment.dto'
-export * from './Workout/index'
+export * from './Workout'
